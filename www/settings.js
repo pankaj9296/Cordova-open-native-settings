ad0557e4dfe23cc0bff62919f9b4bcddf96af0af
pankaj9296 <pankaj9296@gmail.com>
2016-11-18 20:20:16 +0530

    fix

--- www/settings.js
+++ www/settings.js
@@ -11,20 +11,7 @@ NativeSettings.prototype.openSetting = function(settingName, onsucess, onfail) {
 	exec(onsucess, onfail, "NativeSettings", settingName, []);
 };
 
-var NativeSettings = new NativeSettings();
-module.exports = NativeSettings;
-
 if(!window.plugins) window.plugins = {};
-window.plugins.NativeSettings = new NativeSettings;
+window.plugins.NativeSettings = new NativeSettings();
 
-//Plug in to Cordova
-cordova.addConstructor(function() {
-
-    if (!window.Cordova) {
-        window.Cordova = cordova;
-    };
-
-    if(!window.plugins) window.plugins = {};
-    window.plugins.NativeSettings = new NativeSettings;
-});
-
+console.log('NativeSettings');
